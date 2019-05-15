173c1daf9ac3c7fda1805df800d99ac541b018bc
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-15 09:49:00 +0800

    feature(index): refactor the src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,58 @@
 const loaderUtils = require("loader-utils")
 const matter = require("gray-matter")
 
-const git = require("./packages/git")
+//const git = require("./packages/git")
 
 const md = require("markdown-it")({
   html: true,
 })
 
-const stringify = (src) =>
+interface MatteredMD {
+  attributes: any
+  body: string
+  html: string
+}
+
+const stringify = (src: string) =>
   JSON.stringify(src)
     .replace(/\u2028/g, "\\u2028")
     .replace(/\u2029/g, "\\u2029")
 
-let vueCompiler, vueCompilerStripWith
+let vueCompiler: any, vueCompilerStripWith: any
 try {
   vueCompiler = require("vue-template-compiler")
   vueCompilerStripWith = require("vue-template-es2015-compiler")
 } catch (err) {}
 
-module.exports = function(source) {
-  if (this.cacheable) this.cacheable()
+module.exports = function(source: string) {
+  //if (this.cacheable) this.cacheable()
+  // address TS2339: another way would be the not recommended @ts-ignore
+  if ((this as any).cacheable) {
+    ;(this as any).cacheable()
+  }
 
   const options = loaderUtils.getOptions(this) || {}
 
-  const gm = matter(source)
+  //const gm = matter(source)
+  const { content, data: attributes } = matter(source)
+  const body: string = content as string
+  //const attributes:any = data as any
+
+  const html: string = !!options.markdown
+    ? options.markdown(body)
+    : md.render(body)
 
   //const fm = frontmatter(source)
-  const fm = {
-    body: gm.content,
-    attributes: gm.data,
-  }
+  // TODO: add declaration for gray-matter
+  const fm: MatteredMD = { attributes, body, html }
 
+  /*
   if (options.markdown) {
     fm.html = options.markdown(fm.body)
   } else {
     fm.html = md.render(fm.body)
   }
+  */
 
   let output = `
     body: ${stringify(fm.body)},
@@ -58,7 +75,7 @@ module.exports = function(source) {
     if (compiled.staticRenderFns.length > 0) {
       staticRenderFns = `return ${vueCompilerStripWith(
         `[${compiled.staticRenderFns
-          .map((fn) => `function () { ${fn} }`)
+          .map((fn: string) => `function () { ${fn} }`)
           .join(",")}]`
       )}`
     }
@@ -82,7 +99,7 @@ module.exports = function(source) {
             )};
             this.$options.staticRenderFns = ${vueCompilerStripWith(
               `[${compiled.staticRenderFns
-                .map((fn) => `function () { ${fn} }`)
+                .map((fn: string) => `function () { ${fn} }`)
                 .join(",")}]`
             )};
           }
